d8b959aa9298c3b057d9bf638d50dd13fcd4a236
java <answngusdld@gmail.com>
2022-05-19 13:37:28 +0900

    JDBC

---
+++ Day_0519/src/First_JDBC.java
@@ -0,0 +1,12 @@
+
+public class First_JDBC {
+
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "";
+		String nsrname = "hr";
+		String password = "hr";
+
+	}
+
+}
